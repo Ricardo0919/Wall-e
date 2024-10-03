64f44b2cd6d4cb0f4e63ad1da5901d2f6dce1889
Ricardo0919 <rickisierra03@gmail.com>
2024-10-03 01:23:49 -0600

    Update version 2

--- MQTT_SU_MOTORS/MQTT_SU_MOTORS.ino
+++ MQTT_SU_MOTORS/MQTT_SU_MOTORS.ino
@@ -1,4 +1,4 @@
-#include <WiFi.h>
+ #include <WiFi.h>
 #include <PubSubClient.h>
 
 // Configuración del sensor ultrasónico
@@ -11,6 +11,9 @@
 #define IN3 21
 #define IN4 19
 
+// Buzzer
+#define buzzer 18
+
 // Configuración de la red Wi-Fi
 //Casa
 //const char* ssid = "INFINITUM4B9D_2.4";
@@ -21,52 +24,152 @@ const char* password = "ricki1903#$";
 
 // Configuración del broker MQTT
 //Casa
-//const char* mqttServer = "192.168.1.85";
+const char* mqttServer = "192.168.1.85";
 //TEC
-const char* mqttServer = "10.25.99.111";
+//const char* mqttServer = "10.25.99.111";
 const int mqttPort = 1883;
 
 // Instancias de cliente Wi-Fi y MQTT
 WiFiClient wifiClient;
 PubSubClient mqttClient(wifiClient);
 
+bool isMeasuring = false;  // Variable para controlar si se está midiendo o no
+
+// Variable global para almacenar el comando de giro
+String turnCommand = "";
+
+void forward(){
+  int motorSpeed = 100;
+
+  //Motor1
+  digitalWrite(IN1, motorSpeed);
+  digitalWrite(IN2, 0);
+  //Motor2
+  digitalWrite(IN3, 0);
+  digitalWrite(IN4, motorSpeed);
+}
+
+void turn() {
+  int motorSpeed = 100;
+
+  // Motor1 gira hacia adelante
+  analogWrite(IN1, motorSpeed);
+  analogWrite(IN2, 0);   // Apagar la entrada IN2 para que Motor1 gire hacia adelante
+
+  // Motor2 gira hacia atrás
+  analogWrite(IN3, motorSpeed);   // Apagar la entrada IN3 para que Motor2 gire hacia atrás
+  analogWrite(IN4, 0);
+
+  // Mantener el giro por 4 segundos
+  delay(4000);
+
+  // Detener los motores después de 4 segundos
+  analogWrite(IN1, 0);   // Apagar Motor1
+  analogWrite(IN2, 0);   // Apagar Motor1
+  analogWrite(IN3, 0);   // Apagar Motor2
+  analogWrite(IN4, 0);   // Apagar Motor2
+}
+
+
+// Función start para iniciar el movimiento y revisar el comando del tópico turn
+void start() {
+
+  // Activar medición
+  isMeasuring = true;
+
+  // Verificar el comando recibido del tópico esp32/turn
+  if (turnCommand == "Turn") {
+    turn();  // Si el mensaje es "Turn", llamar a la función turn
+  } else {
+    forward();  // Si el mensaje no es "Turn", avanzar hacia adelante
+  }
+}
+
+void stop() {
+  //Motor1
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, LOW);
+  //Motor2
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4, LOW);
+
+  isMeasuring = false;  // Detener medición
+  mqttClient.publish("esp32/distance", "Measurement has been stopped");
+  mqttClient.publish("esp32/object", "Detection of objects has been stopped");
+}
+
+// Función para medir la distancia con el sensor ultrasónico
+void measureDistance() {
+  digitalWrite(TRIG_PIN, LOW);
+  delayMicroseconds(2);
+  digitalWrite(TRIG_PIN, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(TRIG_PIN, LOW);
+
+  long duration = pulseIn(ECHO_PIN, HIGH, 30000); // Timeout de 30ms
+
+  if (duration > 0) {
+    float distance = (duration / 2.0) * 0.0343;
+
+    if (distance <= 20.0) {
+      Serial.print("Distancia: ");
+      Serial.print(distance);
+      Serial.println(" cm");
+
+      // Publicar la distancia al tópico MQTT
+      String distanceStr = String(distance);
+      mqttClient.publish("esp32/distance", distanceStr.c_str());
+
+      // Publicar la detección del objeto al tópico MQTT
+      mqttClient.publish("esp32/object", "Object not found");
+
+      // Si la distancia está entre 10 cm y 15 cm, hacer sonar el buzzer
+      if (distance >= 10.0 && distance <= 15.0) {
+        mqttClient.publish("esp32/object", "Object found");
+        digitalWrite(buzzer, HIGH);  // Encender el buzzer
+        delay(2000);  // Mantener el buzzer encendido durante 2 segundos
+        digitalWrite(buzzer, LOW);   // Apagar el buzzer después de 2 segundos
+      }
+
+      if (turnCommand == "Turn") {
+        turn();  // Si el comando es "Turn", llamar a la función turn
+      } else {
+        forward();  // Si no, seguir avanzando
+      }
+
+    } else {
+      Serial.println("Out of range");
+      mqttClient.publish("esp32/distance", "Out of range");
+      mqttClient.publish("esp32/object", "Object not found");
+    }
+  }
+}
+
 // Función de callback para manejar mensajes MQTT
 void callback(char* topic, byte* payload, unsigned int length) {
-    // Convierte el payload a un String
-    String message;
-    for (int i = 0; i < length; i++) {
-        message += (char)payload[i];
-    }
-    Serial.print("Mensaje recibido en el tópico [");
-    Serial.print(topic);
-    Serial.print("]: ");
-    Serial.println(message);
+  // Convierte el payload a un String
+  String message;
+  for (int i = 0; i < length; i++) {
+    message += (char)payload[i];
+  }
+  Serial.print("Mensaje recibido en el tópico [");
+  Serial.print(topic);
+  Serial.print("]: ");
+  Serial.println(message);
 
-    // Controlar el motor 1 según el mensaje recibido
-    if (String(topic) == "esp32/movement") {
-        if (message == "Forward") {
-            //Motor1
-            digitalWrite(IN1, HIGH);
-            digitalWrite(IN2, LOW);
-            //Motor2
-            digitalWrite(IN3, HIGH);
-            digitalWrite(IN4, LOW);
-        } else if (message == "Backward") {
-            //Motor1
-            digitalWrite(IN1, LOW);
-            digitalWrite(IN2, HIGH);
-            //Motor2
-            digitalWrite(IN3, LOW);
-            digitalWrite(IN4, HIGH);
-        } else if (message == "Stop") {
-            //Motor1
-            digitalWrite(IN1, LOW);
-            digitalWrite(IN2, LOW);
-            //Motor2
-            digitalWrite(IN3, LOW);
-            digitalWrite(IN4, LOW);
-        }
+  // Controlar el motor y la medición según el mensaje recibido
+  if (String(topic) == "esp32/movement") {
+    if (message == "Start") {
+      start();  // Comenzar medición y mover motores
+    } else if (message == "Stop") {
+      stop();   // Detener motores y medición
     }
+  }
+  
+  if (String(topic) == "esp32/turn") {
+    // Guardar el comando recibido en el tópico esp32/turn
+    turnCommand = message;
+  }
 }
 
 // Conectar al broker MQTT
@@ -77,6 +180,7 @@ void reconnect() {
             Serial.println("Conectado");
             // Suscribirse a los tópicos de los motores
             mqttClient.subscribe("esp32/movement");
+            mqttClient.subscribe("esp32/turn");
         } else {
             Serial.print("Fallido, rc=");
             Serial.print(mqttClient.state());
@@ -93,6 +197,9 @@ void setup() {
     // Configurar el sensor ultrasónico
     pinMode(TRIG_PIN, OUTPUT);
     pinMode(ECHO_PIN, INPUT);
+
+    // Configurar el buzzer
+    pinMode(buzzer, OUTPUT);  // Configurar el buzzer como salida
 
     // Configurar los pines de control de los motores
     pinMode(IN1, OUTPUT);
@@ -113,40 +220,17 @@ void setup() {
     mqttClient.setCallback(callback);
 }
 
+
 void loop() {
-    if (!mqttClient.connected()) {
-        reconnect();
-    }
-    mqttClient.loop();
+  if (!mqttClient.connected()) {
+    reconnect();
+  }
+  mqttClient.loop();
 
-    // Medir la distancia con el sensor ultrasónico
-    digitalWrite(TRIG_PIN, LOW);
-    delayMicroseconds(2);
-    digitalWrite(TRIG_PIN, HIGH);
-    delayMicroseconds(10);
-    digitalWrite(TRIG_PIN, LOW);
+  // Si se está midiendo, continuar midiendo y publicando la distancia
+  if (isMeasuring) {
+    measureDistance();
+  }
 
-    long duration = pulseIn(ECHO_PIN, HIGH, 30000); // Timeout de 30ms
-
-    if (duration > 0) {
-        float distance = (duration / 2.0) * 0.0343;
-
-        if (distance <= 20.0) {
-          Serial.print("Distancia: ");
-          Serial.print(distance);
-          Serial.println(" cm");
-
-          // Publicar la distancia al tópico MQTT
-          String distanceStr = "Distancia: " + String(distance) + " cm";
-          mqttClient.publish("esp32/distance", distanceStr.c_str());
-        } 
-
-        else {
-          Serial.println("Out of range");
-          mqttClient.publish("esp32/distance", "Out of range");
-        }
-
-    }
-
-    delay(500);  // Esperar medio segundo antes de la próxima medición
+  delay(500);  // Esperar medio segundo antes de la próxima medición
 }
