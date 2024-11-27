911cd114876e9badb028a41790b07b9625a834ce
jon <jdra1982@hotmail.com>
2024-11-26 21:52:56 -0600

    Vision_Cam6_UNion_de_2_3

---
+++ WALLE/Vision_Cam6of2and3.py
@@ -0,0 +1,24 @@
+import cv2
+
+def main():
+    url = "http://192.168.137.2:4747/video"  # Asegúrate de que este es el endpoint correcto
+    cap = cv2.VideoCapture(url)
+
+    if not cap.isOpened():
+        print("No se pudo abrir el flujo de video.")
+        return
+
+    while True:
+        ret, frame = cap.read()
+        if not ret:
+            print("No se pudo leer el fotograma.")
+            break
+        cv2.imshow('DroidCam', frame)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    cap.release()
+    cv2.destroyAllWindows()
+
+
+main()
